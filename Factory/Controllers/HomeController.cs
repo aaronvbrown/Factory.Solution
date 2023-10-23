3045c6fea3fe46cc7c6ff4659416c301c1c6d44e
Aaron Brown <worldofaaron@hotmail.com>
2023-10-22 18:13:06 -0700

    update HomeController Index method for M2M

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactorContext _db;
+
+    public HomeController(ToDoListContext db)
+    {
+      _db = db;
+    }
+    
     [HttpGet("/")]
     public ActionResult Index()
     {
+      Engineer[] engineers = _db.Engineers.ToArray[];
+      Machine[] machines = _db.Machines.ToArray[];
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      model.Add("machines", machines);
+      model.Add("engineers", engineers);
       return View();
     }
   }
