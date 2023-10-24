9f486257bbfeabbe921324fbd8537a465dbb21f4
Aaron Brown <worldofaaron@hotmail.com>
2023-10-24 16:06:23 -0700

    disallows blank fields for machines, engineers

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Engineer
   {
     public int EngineerId { get; set; }
+    [Required(ErrorMessage = "Engineer FirstName cannot be empty!")]
     public string FirstName { get; set; }
     public List<EngineerMachine> JoinEntities { get; }
   }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
   public class Machine
   {
     public int MachineId { get; set; }
+    [Required(ErrorMessage = "The machine description cannot be blank!")]
     public string Description { get; set; }
     public List<EngineerMachine> JoinEntities { get; set; }
   }
